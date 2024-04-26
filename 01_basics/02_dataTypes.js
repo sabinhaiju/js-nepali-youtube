646e6e4f7e6fa4971e50308c980ca219caf0ff85
sabinhaiju <85225462+sabinhaiju@users.noreply.github.com>
2024-04-26 04:28:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+let name = "sabin"
+let age = 27
+let isLoggedIn = false
+let state;
+let city = null
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true or false
+// null  => standalone value
+// undefined => representation of empty value
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "Sabin")
+console.log(typeof null)
+console.log(typeof undefined)
